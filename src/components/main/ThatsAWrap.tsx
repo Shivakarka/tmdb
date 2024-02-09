7ba6315d55ff0812fe555ba1bafffe90a3ac1e47
shivakarka <shivakarka@gmail.com>
2024-02-10 00:54:13 +0530

    Add ThatsAWrap banner component and new font styles

---
+++ src/components/main/ThatsAWrap.tsx
@@ -0,0 +1,34 @@
+import ThatsAWrapImg from "../../assets/images/ThatsAWrap.jpg";
+import RightArrow from "../../assets/icons/right-arrow.svg";
+
+const ThatsAWrap = () => {
+  return (
+    <div
+      className="h mx-auto flex h-[300px] items-center justify-start bg-cover bg-center text-white md:h-[330px] md:w-full lg:h-[360px] lg:w-[1300px]"
+      style={{ backgroundImage: `url(${ThatsAWrapImg})`, opacity: 1 }}
+    >
+      <div className="h-full w-full bg-black/70">
+        <div className="ms-1 mt-0 flex flex-col gap-5 px-[30px] py-[40px] md:mt-4 lg:mt-6">
+          <div
+            className="font-bold leading-[3rem] lg:leading-[4rem]  "
+            style={{ fontFamily: "Inter" }}
+          >
+            <h2 className=" bg-gradient-to-br from-green-300 to-blue-700 bg-clip-text text-[40px] text-transparent md:text-[50px] lg:text-6xl">
+              That's a<br />
+              Wrap 2023
+            </h2>
+          </div>
+          <p className="text-[20px]">
+            The best (and worst) of the year from TMDB.
+          </p>
+          <button className="flex w-[150px] items-center justify-center gap-2 rounded-full border-2 px-[16px] py-[8px] ">
+            <h2 className="font-semibold">Check it out </h2>
+            <img src={RightArrow} width={20} height={20} alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThatsAWrap;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import ThatsAWrap from "../components/main/ThatsAWrap";
 import Welcome from "../components/main/Welcome";
 
 const Home = () => {
   return (
     <div>
       <Welcome />
+      <ThatsAWrap />
     </div>
   );
 };
