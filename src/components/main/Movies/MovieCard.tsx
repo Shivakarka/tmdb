013781c4e71a687031bd3f1246ecf1cde1f99c18
shivakarka <shivakarka@gmail.com>
2024-02-11 18:21:50 +0530

    Add AddtoList icon in MovieCard

--- src/components/main/Movies/MovieCard.tsx
+++ src/components/main/Movies/MovieCard.tsx
@@ -18,6 +18,11 @@ const MovieCard = ({
         loading="lazy"
         className="h-[225px] min-w-[150px] rounded-lg object-cover shadow-sm"
       />
+      <div className="absolute right-2 top-4 h-[20px] w-[20px] rounded-[50%] bg-gray-300 bg-opacity-70 hover:bg-[rgb(1,180,229)]">
+        <div className=" flex h-full w-full flex-col items-center justify-end pb-[1px] font-bold text-gray-500 ">
+          ...
+        </div>
+      </div>
       <RatingsBar rating={rating} />
       <div className="flex flex-col pl-3 pt-4">
         <h3 className="font-bold">{title}</h3>
