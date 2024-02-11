84ed9fa1bdb2d417cb77d4df9acbe71cacf63083
shivakarka <shivakarka@gmail.com>
2024-02-11 18:03:02 +0530

    Refactor MovieCard component

--- src/components/main/Movies/MovieCard.tsx
+++ src/components/main/Movies/MovieCard.tsx
@@ -1,8 +1,8 @@
 import {
   formattedReleaseDate,
-  getBorderColor,
 } from "../../../utils/helperFunctions";
 import { MovieList } from "./MovieList";
+import RatingsBar from "./RatingsBar";
 
 const MovieCard = ({
   poster_path,
@@ -10,9 +10,9 @@ const MovieCard = ({
   release_date,
   vote_average,
 }: MovieList) => {
-  const rating = Math.floor(vote_average * 10);
 
-  const borderColor = getBorderColor(rating);
+
+  const rating = Math.floor(vote_average * 10); 
 
   return (
     <div className="relative flex cursor-pointer flex-col gap-2">
@@ -22,30 +22,11 @@ const MovieCard = ({
         loading="lazy"
         className="h-[225px] min-w-[150px] rounded-lg object-cover shadow-sm"
       />
-      <div
-        className={`${borderColor} radial-progress absolute left-3 top-[13em] border border-solid border-[rgb(3,37,65)] bg-[rgb(3,37,65)]`}
-        style={
-          {
-            "--value": rating,
-            "--size": "2.3rem",
-            "--thickness": "3px",
-          } as React.CSSProperties
-        }
-        role="progressbar"
-      >
-        {rating > 0 ? (
-          <span className="text-white">
-            {rating}
-            <sup className="text-[7px]">%</sup>
-          </span>
-        ) : (
-          <span className="text-white">NR</span>
-        )}
-      </div>
+        <RatingsBar rating={rating} />
       <div className="flex flex-col pl-3 pt-4">
         <h3 className="font-bold">{title}</h3>
         <p className="font-normal">{formattedReleaseDate(release_date)}</p>
-      </div>
+      R</div>
     </div>
   );
 };
