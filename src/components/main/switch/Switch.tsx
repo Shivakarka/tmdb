55c1fc948eedb95f318bc5ac5d214708f6ef0ff8
shivakarka <shivakarka@gmail.com>
2024-02-14 19:29:56 +0530

    small UI fixes

--- src/components/main/switch/Switch.tsx
+++ src/components/main/switch/Switch.tsx
@@ -26,7 +26,7 @@ const Switch = ({ items, onToggle, title }: SwitchProps) => {
 
   return (
     <div
-      className={` border-tmdbDarkBlue ${title === "Latest Trailers" && "border-[#1ed5a9] text-white "} relative z-[1] hidden items-center rounded-[30px] border border-solid font-semibold hover:cursor-pointer lg:flex`}
+      className={` relative z-[1] hidden items-center rounded-[30px] border border-solid font-semibold hover:cursor-pointer lg:flex ${title === "Latest Trailers" ? "border-[#1ed5a9] text-white " : "border-tmdbDarkBlue"}`}
     >
       {items.map((item, index) => (
         <div
@@ -35,14 +35,14 @@ const Switch = ({ items, onToggle, title }: SwitchProps) => {
           onClick={() => handleToggle(index)}
           className={`h-8 rounded-[30px] px-5 py-1 ${
             activeIndex === index &&
-            `bg-gradient-to-r from-[#c0fecf] to-[#1ed5a9] bg-clip-text text-transparent ${title === "Latest Trailers" ? "text-tmdbDarkBlue" : ""}`
+            ` ${title === "Latest Trailers" ? "text-tmdbDarkBlue" : "bg-gradient-to-r from-[#c0fecf] to-[#1ed5a9] bg-clip-text text-transparent"}`
           }`}
         >
           {item}
         </div>
       ))}
       <div
-        className={`bg-tmdbDarkBlue absolute z-[-1] h-8 rounded-[30px] transition-all duration-300 ease-in-out ${title === "Latest Trailers" ? " bg-gradient-to-r from-[#c0fecf] to-[#1ed5a9]" : ""}`}
+        className={`absolute z-[-1] h-8 rounded-[30px] bg-tmdbDarkBlue transition-all duration-300 ease-in-out ${title === "Latest Trailers" ? " bg-gradient-to-r from-[#c0fecf] to-[#1ed5a9]" : ""}`}
         style={{
           left:
             activeIndex > 0
