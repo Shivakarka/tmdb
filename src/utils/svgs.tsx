e72e07519103478c0952ba1d7226792d519fdfe3
shivakarka <shivakarka@gmail.com>
2024-02-21 02:26:03 +0530

    Fixed small warnings related to casing in attributes

--- src/utils/svgs.tsx
+++ src/utils/svgs.tsx
@@ -8,7 +8,7 @@ const Graph = () => {
           d="M2 3 L 200 3 200 48 2 48Z"
           stroke="none"
           fill="#fff"
-          fill-opacity="0"
+          fillOpacity="0"
         ></path>
         <g>
           <g></g>
@@ -18,7 +18,7 @@ const Graph = () => {
               className="hidden"
               d="M2 3 L 2 48"
               stroke="#8e8e8e"
-              stroke-width="2"
+              strokeWidth="2"
               fill="none"
             ></path>
           </g>
@@ -28,7 +28,7 @@ const Graph = () => {
               <path
                 d="M16.143 44.143 L 44.429 30 72.714 14.571 101 8.143 129.286 8.143 157.571 12 185.857 9.429"
                 stroke="#200b0b"
-                stroke-width="2"
+                strokeWidth="2"
                 fill="none"
               ></path>
             </g>
