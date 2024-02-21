1c3c17122646973360d5da0012b6d5e09fdd0943
shivakarka <shivakarka@gmail.com>
2024-02-22 00:29:10 +0530

    Update grid column proportions on large screens

--- src/components/movieDetails/movieDetailsMain/MovieDetailsMain.tsx
+++ src/components/movieDetails/movieDetailsMain/MovieDetailsMain.tsx
@@ -8,7 +8,7 @@ const MovieDetailsMain = () => {
   const { id } = useParams<{ id: string }>();
 
   return (
-    <main className="mx-auto grid h-fit grid-cols-1 overflow-x-hidden px-[30px] py-[40px] md:min-h-[1500px] md:grid-cols-[60%,40%] lg:grid-cols-[3.8fr,1fr] xl:w-[1400px]">
+    <main className="mx-auto grid h-fit grid-cols-1 overflow-x-hidden px-[30px] py-[40px] md:min-h-[1500px] md:grid-cols-[60%,40%] lg:grid-cols-[80%,20%] xl:w-[1400px]">
       <section className="flex flex-col gap-3 bg-red-200 p-3">
         <CastCards id={id} platform={"movie"} />
         <hr />
