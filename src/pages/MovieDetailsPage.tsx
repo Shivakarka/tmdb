d677b400cc1ba9477c65fc81e87585f16e96de0e
shivakarka <shivakarka@gmail.com>
2024-02-22 00:25:20 +0530

    Add automatic scroll to top upon landing at these pages

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import MovieDetailsHeader from "../components/movieDetails/movieDetailsHeader/MovieDetailsHeader";
 import MovieDetailsMain from "../components/movieDetails/movieDetailsMain/MovieDetailsMain";
 
 const MovieDetailsPage = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, []);
+
   return (
     <>
       <MovieDetailsHeader />
--- src/pages/TvDetailsPage.tsx
+++ src/pages/TvDetailsPage.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import TvDetailsHeader from "../components/tvDetails/tvDetailsHeader/TvDetailsHeader";
 import TvDetailsMain from "../components/tvDetails/tvDetailsMain/TvDetailsMain";
 
 const TvDetailsPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <TvDetailsHeader />
