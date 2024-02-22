24189d78979200053df5781703db459de4a8d09f
shivakarka <shivakarka@gmail.com>
2024-02-23 00:00:52 +0530

    fix z-index value

--- src/components/blurEffect/WhiteBlurEffect.tsx
+++ src/components/blurEffect/WhiteBlurEffect.tsx
@@ -1,7 +1,7 @@
 const WhiteBlurEffect = () => {
   return (
     <div
-      className="absolute right-[-2px] top-0 z-[10000] h-full w-[70px]"
+      className="absolute right-[-2px] top-0 z-[10] h-full w-[70px]"
       style={{
         backgroundImage: "linear-gradient(to right,rgba(255,255,255,0),#fff)",
       }}
--- src/components/trailer/TrailerModal.tsx
+++ src/components/trailer/TrailerModal.tsx
@@ -9,7 +9,7 @@ const TrailerModal = ({
   isModalOpen,
   setIsModalOpen,
   trailerKey,
-  trailerTitle="Play Trailer",
+  trailerTitle = "Play Trailer",
 }: TrailerModalProps) => {
   const closeModal = () => {
     setIsModalOpen(false);
@@ -21,15 +21,13 @@ const TrailerModal = ({
       className="modal bg-black bg-opacity-50"
       open={isModalOpen}
     >
-      <div className="top-15 modal-box absolute z-[10] h-[280px] w-[300px] overflow-hidden bg-black text-white md:h-[500px] md:min-w-[750px] lg:h-[83%] lg:min-w-[1300px]">
-        <form method="dialog">
-          <button
-            className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2 hover:bg-gray-700"
-            onClick={closeModal}
-          >
-            ✕
-          </button>
-        </form>
+      <div className="top-15 modal-box absolute z-[100] h-[280px] w-[300px] overflow-hidden bg-black text-white md:h-[500px] md:min-w-[750px] lg:h-[83%] lg:min-w-[1300px]">
+        <button
+          className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2 hover:bg-gray-700"
+          onClick={closeModal}
+        >
+          ✕
+        </button>
         <h3 className="mb-3 text-lg font-bold">{trailerTitle}</h3>
         <p className="mx-auto">
           <iframe
