d16b60c6e67e68137b40d8510198c488e46d9670
shivakarka <shivakarka@gmail.com>
2024-02-24 17:35:40 +0530

    Add ReviewCard component to SocialSection

---
+++ src/components/socialSection/ReviewCard.tsx
@@ -0,0 +1,69 @@
+import StarIcon from "../../assets/icons/star.svg";
+import NoImage from "../../assets/images/noImage.svg";
+import { formatDate } from "../../utils/helperFunctions";
+import { truncateReviewContent } from "./truncateReview";
+
+type ReviewCardProps = {
+    avatar_path: string;
+    author: string;
+    rating: number;
+    created_at: string;
+    content: string;
+};
+
+const ReviewCard = ({
+  avatar_path,
+  author,
+  rating,
+  created_at,
+  content,
+}: ReviewCardProps) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex h-fit w-[78vw] flex-col gap-3 rounded-lg p-4 shadow-lg sm:w-full">
+        <div className="flex gap-3">
+          {avatar_path ? (
+            <img
+              src={`https://media.themoviedb.org/t/p/w45_and_h45_face/${avatar_path}`}
+              alt="avatar"
+              loading="lazy"
+              className="h-[45px] w-[45px] rounded-full"
+            />
+          ) : (
+            <img
+              src={NoImage}
+              alt="No Image"
+              loading="lazy"
+              className="h-[45px] w-[45px] rounded-full"
+            />
+          )}
+          <div className="flex flex-col gap-0">
+            <h3 className="text-xl font-bold">A review by {author}</h3>
+            <div className="flex w-fit items-center gap-3   text-black ">
+              <div className="flex h-4 items-center justify-center gap-1 rounded-full bg-black px-1 py-[1px] text-white">
+                <img
+                  src={StarIcon}
+                  alt="Star icon"
+                  className="h-3 w-3 "
+                  loading="lazy"
+                />
+                <p className="text-sm">{rating}</p>
+              </div>
+              <p className="text-sm">
+                Written by {author} on {formatDate(created_at)}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="text-md">{truncateReviewContent(content)}</p>
+        </div>
+      </div>
+      <p className="cursor-pointer text-lg font-bold hover:opacity-50">
+        See all Reviews
+      </p>
+    </div>
+  );
+};
+
+export default ReviewCard;
--- src/components/socialSection/SocialSection.tsx
+++ src/components/socialSection/SocialSection.tsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useReviews } from "../../utils/customHooks";
-import StarIcon from "../../assets/icons/star.svg";
-import NoImage from "../../assets/images/noImage.svg";
-import { formatDate } from "../../utils/helperFunctions";
-import { truncateReviewContent } from "./truncateReview";
+import ReviewCard from "./ReviewCard";
 
 const SocialSection = ({ platform }: { platform: string }) => {
   const [activeTab, setActiveTab] = useState(0);
@@ -14,7 +11,7 @@ const SocialSection = ({ platform }: { platform: string }) => {
 
   return (
     <div className="my-4 flex flex-col gap-4">
-      <div className="flex items-start justify-start gap-10">
+      <div className="flex items-start justify-start gap-5 md:gap-10">
         <p className="text-2xl font-semibold">Social</p>
         <div role="tablist" className="tabs tabs-bordered relative">
           <input
@@ -29,62 +26,19 @@ const SocialSection = ({ platform }: { platform: string }) => {
           />
           <div
             role="tabpanel"
-            className="tab-content relative left-[-6.5rem] top-1 mt-1 h-fit w-[85vw] min-w-[320px] sm:w-[88vw] md:h-fit md:w-[55vw] md:min-w-[400px] lg:w-[74vw] xl:w-[1050px]"
+            className="tab-content relative left-[-5rem] top-1 mt-1 h-fit w-[85vw] min-w-[320px] sm:w-[88vw] 
+            md:left-[-6.5rem] md:h-fit md:w-[55vw] md:min-w-[400px] lg:w-[74vw] xl:w-[1050px]"
           >
             {ReviewsData?.results?.length > 0 ? (
-              <div className="flex flex-col gap-3">
-                <div className="flex h-fit w-full flex-col gap-3 rounded-lg  p-4 shadow-lg">
-                  <div className="flex gap-3">
-                    {ReviewsData?.results?.[0]?.author_details?.avatar_path ? (
-                      <img
-                        src={`https://media.themoviedb.org/t/p/w45_and_h45_face/${ReviewsData?.results?.[0]?.author_details?.avatar_path}`}
-                        alt="avatar"
-                        loading="lazy"
-                        className="h-[45px] w-[45px] rounded-full"
-                      />
-                    ) : (
-                      <img
-                        src={NoImage}
-                        alt="No Image"
-                        loading="lazy"
-                        className="h-[45px] w-[45px] rounded-full"
-                      />
-                    )}
-                    <div className="flex flex-col gap-0">
-                      <h3 className="text-xl font-bold">
-                        A review by {ReviewsData?.results?.[0]?.author}
-                      </h3>
-                      <div className="flex w-fit items-center gap-3   text-black ">
-                        <div className="flex h-4 items-center justify-center gap-1 rounded-full bg-black px-1 py-[1px] text-white">
-                          <img
-                            src={StarIcon}
-                            alt="Star icon"
-                            className="h-3 w-3 "
-                            loading="lazy"
-                          />
-                          <p className="text-sm">
-                            {ReviewsData?.results?.[0]?.author_details?.rating}
-                          </p>
-                        </div>
-                        <p className="text-sm">
-                          Written by {ReviewsData?.results?.[0]?.author} on{" "}
-                          {formatDate(ReviewsData?.results?.[0]?.created_at)}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <p className="text-md">
-                      {truncateReviewContent(
-                        ReviewsData?.results?.[0]?.content,
-                      )}
-                    </p>
-                  </div>
-                </div>
-                <p className="cursor-pointer text-lg font-bold hover:opacity-50">
-                  See all Reviews
-                </p>
-              </div>
+              <ReviewCard
+                avatar_path={
+                  ReviewsData?.results?.[0]?.author_details?.avatar_path
+                }
+                author={ReviewsData?.results?.[0]?.author}
+                rating={ReviewsData?.results?.[0]?.author_details?.rating}
+                created_at={ReviewsData?.results?.[0]?.created_at}
+                content={ReviewsData?.results?.[0]?.content}
+              />
             ) : (
               <div className="flex flex-col gap-4 p-3">
                 <p>
@@ -109,7 +63,8 @@ const SocialSection = ({ platform }: { platform: string }) => {
           />
           <div
             role="tabpanel"
-            className="tab-content relative left-[-5rem] top-1 h-[150px] w-[300px]  md:h-[150px] md:w-[120%] lg:w-[700px] xl:w-[980px]"
+            className="tab-content relative left-[-5rem] top-1 h-[150px] w-[300px] 
+            md:h-[150px] md:w-[120%] lg:w-[700px] xl:w-[980px]"
           >
             <div className="flex flex-col gap-4 p-3">
               <p>
